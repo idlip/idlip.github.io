4e02525d947a93245c5860a11a873bcee514bb81
Dilip <igoldlip@gmail.com>
2023-03-29 15:37:17 +0530

    updated script to git push too

--- sync-tilde.sh
+++ sync-tilde.sh
@@ -3,7 +3,8 @@
 # To sync it with tildevarsh and make it clone of github deployment
 
 hugo &&
-    rsync --delete -vae ssh public/* idlip@tildevarsh.in:~/public_html &&
-    rm -rf public
+rsync --delete -vae ssh public/* idlip@tildevarsh.in:~/public_html &&
+rm -rf public &&
+git push
 
 exit 0
